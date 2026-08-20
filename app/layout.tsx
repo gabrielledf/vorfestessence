bf66baee44e6ccb50ad311c1849c1ee8419182d1
gabrielledf <gabrielledf@mpf.mp.br>
2026-08-20 10:46:43 -0300

    Ajustes dos ícones

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,11 +22,10 @@ export const metadata: Metadata = {
   generator: 'v0.app',
   icons: {
     icon: [
-      { url: '/icon-light-32x32.png', media: '(prefers-color-scheme: light)' },
-      { url: '/icon-dark-32x32.png', media: '(prefers-color-scheme: dark)' },
-      { url: '/icon.svg', type: 'image/svg+xml' },
+      { url: '/faviconL.png', media: '(prefers-color-scheme: light)' },
+      { url: '/faviconB.png', media: '(prefers-color-scheme: dark)' },
     ],
-    apple: '/apple-icon.png',
+    apple: '/faviconL.png',
   },
 }
 
